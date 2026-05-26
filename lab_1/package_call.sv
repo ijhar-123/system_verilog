b778d9c21231fe8cea323e649ff2cc6b37e2e54c
ijhar-123 <123933534+ijhar-123@users.noreply.github.com>
2026-05-26 12:09:27 +0530

    Add package_call module with import tests

--- lab_1/package_call.sv
+++ lab_1/package_call.sv
@@ -1,1 +1,28 @@
 
+module package_call;
+
+//wild card import by *
+initial begin: wildard_test
+import constants_pkg::*;
+$display("[wild card import] module compiled successfully with WIDTH=%0d and DEPTH=%0d",WIDTH,DEPTH);
+end
+
+//Legitimate Referencing Methods
+initial begin:specific_import_width
+import constants_pkg::WIDTH;
+$display("specific import using refrencing WIDTH=%0d",WIDTH);
+//$display("specific import using refrencing DEPTH=%0d",DEPTH);
+end
+
+
+initial begin:specific_import_depth
+import constants_pkg::DEPTH;
+//$display("specific import using refrencing WIDTH=%0d",WIDTH);
+$display("specific import using refrencing DEPTH=%0d",DEPTH);
+end
+
+initial begin : scope_resolution_test
+$display("[SCOPE RESOLUTION] WIDTH=%0d and DEPTH=%0d",constants_pkg::WIDTH, constants_pkg::DEPTH);
+end
+
+endmodule
